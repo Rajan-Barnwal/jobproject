438208f0040dc8a10d3f867d6e67bae9fc962bb7
DEBASMI_BASU <83003226+debasmi@users.noreply.github.com>
2024-05-11 15:37:42 +0530

    Update script.js
    for navigation

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.getElementById('nav-toggle');
+    const navLinks = document.getElementById('nav-list');
+
+    navToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
 
